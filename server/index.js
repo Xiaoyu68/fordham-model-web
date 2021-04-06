353ab1dab5748cec44c6f96951081c7129f22b05
Xiaoyu68 <52010750+Xiaoyu68@users.noreply.github.com>
2021-04-06 11:42:58 -0400

    hi

--- server/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-const path = require('path');
-var app = express();
-
-app.set('port', (process.env.PORT || 8081))
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
